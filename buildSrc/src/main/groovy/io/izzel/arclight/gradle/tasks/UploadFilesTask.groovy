d15a135d5f56c47d391d6a42b2d0876206d5070c
Hiosk <mendesleonardo907@gmail.com>
2024-09-01 13:08:50 -0300

    Trocando o upload para localhost

--- buildSrc/src/main/groovy/io/izzel/arclight/gradle/tasks/UploadFilesTask.groovy
+++ buildSrc/src/main/groovy/io/izzel/arclight/gradle/tasks/UploadFilesTask.groovy
@@ -41,8 +41,8 @@ abstract class UploadFilesTask extends DefaultTask {
         }
     }
 
-    private static final String OBJECTS = "https://files.hypoglycemia.icu/v1/objects/%s"
-    private static final String FILES = "https://files.hypoglycemia.icu/v1/files%s"
+    private static final String OBJECTS = "http://localhost:3000/v1/objects/%s"
+    private static final String FILES = "http://localhost:3000/v1/files/%s"
 
     private void uploadOne(File file) {
         def sha1 = Utils.sha1(file)
